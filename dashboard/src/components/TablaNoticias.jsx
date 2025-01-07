431965be0092a8ff7ebfea5a3a0d880cfa4014ea
Fran <franbermejo.dam@gmail.com>
2025-01-07 17:29:55 +0100

    Eliminar noticias

--- dashboard/src/components/TablaNoticias.jsx
+++ dashboard/src/components/TablaNoticias.jsx
@@ -12,6 +12,30 @@ const TablaNoticias = () => {
         .catch(error => console.error('Error:', error));
     }, []);
 
+    const formatearFecha = (fecha) => {
+        const opciones = { year: 'numeric', month: 'long', day: 'numeric' };
+        return new Date(fecha).toLocaleDateString('es-ES', opciones);
+    };
+
+    const eliminarNoticia = async (id) => {
+        try {
+            const respuesta = await fetch(`http://127.0.0.1:8000/noticias/${id}/`, {
+                method: 'DELETE'
+            });
+            if (respuesta.ok) {
+                console.log('Noticia eliminada:', id);
+                alert("Noticia eliminada con éxito.");
+                setData(data.filter(noticia => noticia.id !== id));
+            } else {
+                console.error('Error al eliminar la noticia:', await respuesta.text());
+                alert("Error al eliminar la noticia.");
+            }
+        } catch (e) {
+            console.error('Error al eliminar la noticia:', e);
+            alert("Error al eliminar la noticia.");
+        }
+    }
+
     return (
         <table className="w-full border-collapse border border-gray-300">
             <thead>
@@ -27,9 +51,13 @@ const TablaNoticias = () => {
                     <tr key={index}>
                         <td className="border border-gray-300 p-2">{fila.titulo}</td>
                         <td className="border border-gray-300 p-2">{fila.descripcion}</td>
-                        <td className="border border-gray-300 p-2">{fila.fecha}</td>
+                        <td className="border border-gray-300 p-2">{formatearFecha(fila.fecha)}</td>
                         <td className="border border-gray-300 p-2">
                             <img src={fila.imagen} className="w-24" alt="Imagen de noticia"/>
+                        </td>
+                        <td>
+                            <button className="bg-blue-950 text-white rounded-lg p-2 hover:bg-blue-700 transition-colors" onClick={() => eliminarNoticia(fila.id)}>Editar</button>
+                            <button className="bg-red-500 text-white rounded-lg p-2 ml-2 hover:bg-red-400 transition-colors" onClick={() => eliminarNoticia(fila.id)}>Borrar</button>
                         </td>
                     </tr>
                 ))}
