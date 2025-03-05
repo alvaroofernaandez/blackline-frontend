666cff9c98bd5a3e7d3b63cf87f165eaf089639e
Fran <franbermejo.dam@gmail.com>
2025-03-05 22:04:18 +0100

    Actualizo el noticiero

--- maezink-front/src/components/NoticieroComponent.jsx
+++ maezink-front/src/components/NoticieroComponent.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
+import Noticia from './Noticia';
 
 const Noticiero = () => {
     return (
-        <div>
-            <div>
-                <p>Titulo</p>
-                <p>Descripcion</p>
-                <p>Fecha</p>
-            </div>
+        <div className='flex flex-col gap-12 max-w-4xl mx-auto my-32'>
+        <h1 className='text-4xl text-white font-bold'>Noticias</h1>
+        <hr></hr>
+            <Noticia client="load" />
+            <Noticia client="load"/>
+            <Noticia />
+            <Noticia />
+            <Noticia />
         </div>
     );
 }
