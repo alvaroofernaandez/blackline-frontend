d06face6220747a2cac0ea04b566cca7a703e905
Fran <franbermejo.dam@gmail.com>
2025-03-05 19:13:42 +0100

    Cambio en el header y creacion de la página de noticiero

---
+++ maezink-front/src/components/NoticieroComponent.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Noticiero = () => {
+    return (
+        <div>
+            <div>
+                <p>Titulo</p>
+                <p>Descripcion</p>
+                <p>Fecha</p>
+            </div>
+        </div>
+    );
+}
+
+export default Noticiero;
