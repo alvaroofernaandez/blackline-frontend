a6e64bfabcf1845f4bec38a5049b66dba5e47884
Fran <franbermejo.dam@gmail.com>
2025-03-23 16:57:03 +0100

    actualización del apartado de noticias, añado para que se pueda enviar parametros

--- maezink-front/src/components/Noticia.jsx
+++ maezink-front/src/components/Noticia.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
-const Noticia = () => {
+const Noticia = ({ titulo, descripcion, fecha }) => {
     return (
         <div className="border-2 border-white p-8 rounded-xl bg-black bg-opacity-20 w-full transition duration-500 ease-in-out shadow-xl hover:scale-105 hover:bg-opacity-40 hover:border-gray-400">
-            <h2 className="text-2xl text-white font-semibold">Título de la Noticia</h2>
-            <p className="text-lg text-gray-200 mb-4">Descripción breve de la noticia, manteniendo un diseño limpio y minimalista.</p>
-            <p className="text-md text-gray-200">Fecha: 2023-12-01</p>
+            <h2 className="text-2xl text-white font-semibold">{titulo}</h2>
+            <p className="text-lg text-gray-200 mb-4">{descripcion}</p>
+            <p className="text-md text-gray-200">Fecha: {fecha}</p>
         </div>
     );
 }
--- maezink-front/src/components/NoticieroComponent.jsx
+++ maezink-front/src/components/NoticieroComponent.jsx
@@ -6,14 +6,10 @@ const noticiasPorPagina = 3; // Número de noticias por página
 const Noticiero = () => {
     const [paginaActual, setPaginaActual] = useState(1);
     const noticias = [
-        { id: 1, client: "load" },
-        { id: 2, client: "load" },
-        { id: 3 },
-        { id: 4 },
-        { id: 5 },
-        { id: 6 },
-        { id: 7 },
-        { id: 8 },
+        { id: 1, titulo: "Título 1", descripcion: "Descripción 1", fecha: "Fecha 1" },
+        { id: 2, titulo: "Título 2", descripcion: "Descripción 2", fecha: "Fecha 2" },
+        { id: 3, titulo: "Título 3", descripcion: "Descripción 3", fecha: "Fecha 3" },
+        { id: 4, titulo: "Título 4", descripcion: "Descripción 4", fecha: "Fecha 4" }
     ];
 
     const totalPaginas = Math.ceil(noticias.length / noticiasPorPagina);
@@ -25,7 +21,7 @@ const Noticiero = () => {
             <h1 className='text-4xl text-white font-bold'>Noticias</h1>
             <hr />
             {noticiasPaginadas.map(noticia => (
-                <Noticia key={noticia.id} client={noticia.client} />
+                <Noticia key={noticia.id} titulo={noticia.titulo} descripcion={noticia.descripcion} fecha={noticia.fecha} />
             ))}
             
             <div className='flex justify-center gap-4'>
@@ -38,7 +34,7 @@ const Noticiero = () => {
                 </button>
                 <span className='text-white'>{paginaActual} / {totalPaginas}</span>
                 <button
-                    className="px-4 py-2  text-white rounded border enabled:border-white disabled:bg-gray-400 disabled:border-gray-800"
+                    className="px-4 py-2 text-white rounded border enabled:border-white disabled:bg-gray-400 disabled:border-gray-800"
                     onClick={() => setPaginaActual(prev => Math.min(prev + 1, totalPaginas))}
                     disabled={paginaActual === totalPaginas}
                 >
