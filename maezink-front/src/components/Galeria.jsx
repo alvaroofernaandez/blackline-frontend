903ffb4508dfe54ea92ebadfa0604c8b12cb7ef4
Fran <franbermejo.dam@gmail.com>
2025-03-27 23:49:18 +0100

    Correccion de hover de galeria

--- maezink-front/src/components/Galeria.jsx
+++ maezink-front/src/components/Galeria.jsx
@@ -6,13 +6,13 @@ const Galeria = ({ images }) => {
 
   return (
     <>
-      <div className="grid columns-1 sm:columns-2 md:columns-3 lg:columns-4 gap-4 space-y-4">
+      <div className="grid columns-1 sm:columns-2 md:columns-3 lg:columns-4 gap-4 ">
         {images.map((file, index) => (
           <img
             key={index}
             src={`/galeria/${file}`}
             alt={`Tatuaje ${index + 1}`}
-            className="w-full rounded-xl shadow-2xl break-inside-avoid object-cover hover:scale-110 transition-all duration-300 cursor-pointer"
+            className="w-full rounded-xl shadow-2xl break-inside-avoid object-cover hover:scale-105 transition-all duration-300 cursor-pointer"
             loading="lazy"
             onClick={() => setSelectedImage(`/galeria/${file}`)}
           />
