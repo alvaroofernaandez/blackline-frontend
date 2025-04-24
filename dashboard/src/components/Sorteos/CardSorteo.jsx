484f174d8584d72278e62f907682681a545b5a22
alvaroofernaandez <alvaroofernaandez@gmail.com>
2025-04-24 18:25:12 +0200

    Añade funcionalidad para seleccionar un ganador en el sorteo y mejora la presentación de fechas y estado del sorteo en el componente CardSorteo.

--- dashboard/src/components/Sorteos/CardSorteo.jsx
+++ dashboard/src/components/Sorteos/CardSorteo.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import Alert from "../Alert";
 import { FaRegTrashAlt } from "react-icons/fa";
 import { AiFillEdit } from "react-icons/ai";
+import { GiPodiumWinner } from "react-icons/gi";
 
 const CardSorteo = ({
   id,
@@ -43,6 +44,26 @@ const CardSorteo = ({
     }
   };
 
+  const seleccionarGanador = async (id) => {
+    try {
+      const respuesta = await fetch(
+        `http://127.0.0.1:8000/api/sorteos_seleccionar_ganador/${id}/`,
+        {
+          method: "PATCH",
+        }
+      );
+      if (respuesta.ok) {
+        setTimeout(() => {
+          window.location.reload();
+        }, 0);
+      } else {
+        setAlerta({ type: "error", message: "Error al seleccionar el ganador." });
+      }
+    } catch (e) {
+      setAlerta({ type: "error", message: "Error al seleccionar el ganador." });
+    }
+  }
+
   return (
     <div className="bg-[#262626] rounded-2xl shadow-md p-6 border border-gray-700 hover:shadow-lg transition-shadow flex flex-col justify-between h-full">
       {alerta && (
@@ -56,17 +77,25 @@ const CardSorteo = ({
         <h2 className="text-4xl font-bold text-white text-center mb-2">{titulo}</h2>
         <hr class="border-neutral-700 mb-6" />
         <p className="text-gray-300 mb-4">Descripción: {descripcion}</p>
+        <hr class="border-neutral-700 mb-6" />
 
         <div className="grid gap-3 justify-between text-sm text-gray-400 mb-4">
           <span>
-            🕒 Inicio:{" "}
+            🕒 Fecha de inicio:{" "}
             <strong className="text-gray-200">{formatDate(fecha_inicio)}</strong>
           </span>
           <span>
-            🕛 Fin:{" "}
+            🕛 Fecha de finalización:{" "}
             <strong className="text-gray-200">{formatDate(fecha_fin)}</strong>
           </span>
         </div>
+
+        <p className="text-gray-200 font-medium mb-4">
+          🔷 Estado del sorteo: {" "}
+          <span className="text-green-400 font-semibold">
+            {estado}
+          </span>
+        </p>
 
         <p className="text-gray-200 font-medium mb-4">
           🎉 Ganador:{" "}
@@ -76,7 +105,7 @@ const CardSorteo = ({
         </p>
 
         <div className="mb-4">
-          <h3 className="text-md font-semibold text-white mb-1">🎁 Premios</h3>
+          <h3 className="text-md font-semibold text-white mb-1">🎁 Premios:</h3>
           <ul className="list-disc list-inside text-gray-300">
             {premios.length > 0 ? (
               premios.map((premio, index) => <li key={index}>{premio}</li>)
@@ -88,17 +117,8 @@ const CardSorteo = ({
 
         <div>
           <h3 className="text-md font-semibold text-white mb-1">
-            👥 Participantes
+            👥 Nº de participantes: {participantes.length}
           </h3>
-          <ul className="list-disc list-inside text-gray-300">
-            {participantes.length > 0 ? (
-              participantes.map((p, index) => (
-                <li key={index}>{p.instagram_username}</li>
-              ))
-            ) : (
-              <li>No hay participantes</li>
-            )}
-          </ul>
         </div>
       </div>
 
@@ -114,6 +134,12 @@ const CardSorteo = ({
           onClick={() => eliminarSorteo(id)}
         >
           <FaRegTrashAlt className="size-6" />
+        </button>
+        <button
+          className="bg-green-500 hover:scale-105 transition-all duration-500 w-full text-white size-10 items-center justify-items-center rounded-lg"
+          onClick={() => seleccionarGanador(id)}
+        >
+          <GiPodiumWinner className="size-6" />
         </button>
       </div>
     </div>
