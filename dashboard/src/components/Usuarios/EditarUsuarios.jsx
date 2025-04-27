dd4f8b50ee371420e824219aa9c13189d5a6e40b
alvaroofernaandez <alvaroofernaandez@gmail.com>
2025-04-27 18:32:23 +0200

    feat: Añade funcionalidad para editar y listar usuarios, incluyendo validación de formularios y gestión de errores

---
+++ dashboard/src/components/Usuarios/EditarUsuarios.jsx
@@ -0,0 +1,126 @@
+import { useState, useEffect } from "react";
+import { toast } from "sonner";
+import { z } from "zod";
+
+const usuarioSchema = z.object({
+  username: z.string().min(1, "El nombre de usuario es obligatorio"),
+  email: z.string().email("El correo debe ser válido"),
+  role: z.string().min(1, "El rol es obligatorio"),
+});
+
+const EditarUsuario = ({ id }) => {
+  const [formData, setFormData] = useState({
+    username: "",
+    email: "",
+    role: "",
+  });
+
+  useEffect(() => {
+    const obtenerUsuario = async () => {
+      try {
+        const respuesta = await fetch(`http://127.0.0.1:8000/api/usuarios/${id}/`);
+        if (respuesta.ok) {
+          const datos = await respuesta.json();
+          setFormData(datos[0]);
+        } else {
+          throw new Error("Error al cargar el usuario.");
+        }
+      } catch (error) {
+        console.error(error);
+        toast.error("No se pudo cargar el usuario.");
+      }
+    };
+
+    if (id) obtenerUsuario();
+  }, [id]);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const actualizarUsuario = async (e) => {
+    e.preventDefault();
+    try {
+      usuarioSchema.parse(formData);
+      const respuesta = await fetch(`http://127.0.0.1:8000/api/usuarios/${id}/`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      if (respuesta.ok) {
+        toast.success("Usuario actualizado con éxito.");
+        setTimeout(() => {
+          window.location.href = "/usuarios";
+        }, 500);
+      } else {
+        console.error("Error al actualizar el usuario:", respuesta.statusText);
+        throw new Error("Error al actualizar el usuario.");
+      }
+    } catch (error) {
+      console.error(error);
+      toast.error(error.message || "Error al actualizar el usuario.");
+    }
+  };
+
+  return (
+    <div className="p-4">
+      <h2 className="text-4xl text-center font-bold mb-4">Editar Usuario</h2>
+      <hr />
+      <form onSubmit={actualizarUsuario} className="max-w-[50%] mx-auto mt-20">
+        <label htmlFor="username" className="block mb-2">
+          Nombre de Usuario:
+        </label>
+        <input
+          type="text"
+          id="username"
+          name="username"
+          value={formData.username}
+          onChange={handleChange}
+          className="border border-gray-300 text-black rounded-lg p-2 w-full mb-4"
+          required
+        />
+
+        <label htmlFor="email" className="block mb-2">
+          Correo:
+        </label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          value={formData.email}
+          onChange={handleChange}
+          className="border border-gray-300 text-black rounded-lg p-2 w-full mb-4"
+          required
+        />
+
+        <label htmlFor="role" className="block mb-2">
+          Rol:
+        </label>
+        <input
+          type="text"
+          id="role"
+          name="role"
+          value={formData.role}
+          onChange={handleChange}
+          className="border border-gray-300 text-black rounded-lg p-2 w-full mb-4"
+          required
+        />
+
+        <button
+          type="submit"
+          className="bg-neutral-900 text-white rounded-lg p-2 hover:bg-neutral-800 w-full mt-10 transition-all"
+        >
+          Actualizar
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default EditarUsuario;
---
+++ dashboard/src/components/Usuarios/TablaUsuarios.jsx
@@ -0,0 +1,124 @@
+import { useEffect, useState } from "react";
+import { toast } from "sonner";
+import { FaRegTrashAlt } from "react-icons/fa";
+import { AiFillEdit } from "react-icons/ai";
+import { z } from "zod";
+import Modal from "../General/Modal";
+
+const usuarioSchema = z.object({
+  id: z.number(),
+  username: z.string().min(1, "El nombre de usuario es obligatorio"),
+  email: z.string().email("El correo debe ser válido"),
+  role: z.string().min(1, "El rol es obligatorio"),
+  instagram_username: z.string().nullable(),
+});
+
+const TablaUsuarios = () => {
+  const [data, setData] = useState([]);
+  const [cargando, setCargando] = useState(true);
+  const [showModal, setShowModal] = useState(false);
+  const [usuarioSeleccionado, setUsuarioSeleccionado] = useState(null);
+
+  useEffect(() => {
+    const obtenerUsuarios = async () => {
+      try {
+        const response = await fetch("http://127.0.0.1:8000/api/usuarios/");
+        const usuarios = await response.json();
+        const validData = usuarios.map((usuario) => usuarioSchema.parse(usuario));
+        setData(validData);
+      } catch (error) {
+        toast.error("Error al cargar los datos: " + error.message);
+        console.error("Error:", error);
+      } finally {
+        setCargando(false);
+      }
+    };
+
+    obtenerUsuarios();
+  }, []);
+
+  const eliminarUsuario = async (id) => {
+    try {
+      const respuesta = await fetch(
+        `http://127.0.0.1:8000/api/usuarios/${id}/`,
+        {
+          method: "DELETE",
+        }
+      );
+      if (respuesta.ok) {
+        toast.success("Usuario eliminado con éxito.");
+        setData(data.filter((usuario) => usuario.id !== id));
+        setShowModal(false);
+      } else {
+        toast.error("Error al eliminar el usuario.");
+      }
+    } catch (e) {
+      toast.error("Error al eliminar el usuario.");
+    }
+  };
+
+  const abrirModal = (id) => {
+    setUsuarioSeleccionado(id);
+    setShowModal(true);
+  };
+
+  if (cargando) return <p className="text-center">Cargando usuarios...</p>;
+  if (data.length === 0)
+    return <p className="text-red-500 text-center">No hay usuarios actualmente, añade un usuario</p>;
+
+  return (
+    <div className="container mx-auto p-4">
+      <h2 className="text-2xl font-semibold text-white mb-4">Lista de Usuarios</h2>
+      <div className="max-h-[700px] overflow-y-auto rounded-xl">
+        <table className="w-full border border-gray-200 shadow-md rounded-lg overflow-hidden">
+          <thead className="bg-neutral-800 text-white sticky top-0 z-10">
+            <tr>
+              <th className="p-3 text-left">Nombre de Usuario</th>
+              <th className="p-3 text-left">Correo</th>
+              <th className="p-3 text-left">Rol</th>
+              <th className="p-3 text-left">Instagram</th>
+              <th className="p-3 text-center">Acciones</th>
+            </tr>
+          </thead>
+          <tbody className="bg-white divide-y divide-gray-200 ">
+            {data.map((fila, index) => (
+              <tr
+                key={index}
+                className={`hover:bg-gray-100 transition ${index % 2 === 0 ? "bg-gray-50" : "bg-white"}`}
+              >
+                <td className="p-3 text-gray-700">{fila.username}</td>
+                <td className="p-3 text-gray-600">{fila.email}</td>
+                <td className="p-3 text-gray-600">{fila.role}</td>
+                <td className="p-3 text-gray-600">{fila.instagram_username || "N/A"}</td>
+                <td className="flex gap-3 justify-center items-center p-8">
+                  <button
+                    className="bg-neutral-400 size-10 justify-items-center hover:scale-105 transition-all duration-500 text-white rounded-lg"
+                    onClick={() => (window.location.href = `/actualizar-usuario/${fila.id}`)}
+                  >
+                    <AiFillEdit className="text-xl" />
+                  </button>
+                  <button
+                    className="bg-neutral-800 size-10 justify-items-center hover:scale-105 transition-all duration-500 text-white rounded-lg"
+                    onClick={() => abrirModal(fila.id)}
+                  >
+                    <FaRegTrashAlt className="text-xl" />
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+
+      {showModal && (
+        <Modal
+          setShowModal={setShowModal}
+          eliminarObjeto={eliminarUsuario}
+          id={usuarioSeleccionado}
+        />
+      )}
+    </div>
+  );
+};
+
+export default TablaUsuarios;
