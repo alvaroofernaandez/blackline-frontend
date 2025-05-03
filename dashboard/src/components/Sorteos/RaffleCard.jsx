1a09e8a1479e65701ab46aedbaf12945d9170426
alvaroofernaandez <alvaroofernaandez@gmail.com>
2025-05-03 16:24:51 +0200

    feat: agregar botón para navegar a la lista de participantes en la tarjeta de sorteos; mejorar estilos y tamaño del título

--- dashboard/src/components/Sorteos/RaffleCard.jsx
+++ dashboard/src/components/Sorteos/RaffleCard.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { FaRegTrashAlt, FaGift } from "react-icons/fa";
 import { AiFillEdit } from "react-icons/ai";
 import { GiPodiumWinner } from "react-icons/gi";
+import { FaUsers } from "react-icons/fa";
 import Modal from "../General/Modal";
 import { z } from "zod";
 import { format } from "date-fns";
@@ -51,8 +52,8 @@ const CardSorteo = (props) => {
   };
 
   return (
-    <div className="bg-gradient-to-br from-neutral-600 to-neutral-500 dark:from-neutral-900 dark:to-neutral-800 hover:scale-105 shadow-lg transition-transform duration-300 text-white rounded-lg p-6 flex flex-col gap-4">
-      <h2 className="text-xl font-semibold">{titulo}</h2>
+    <div className="bg-gradient-to-br from-neutral-600 to-neutral-500 dark:from-neutral-900 dark:to-neutral-800 hover:-translate-y-1 shadow-lg transition-transform duration-300 text-white rounded-lg p-6 flex flex-col gap-4">
+      <h2 className="text-3xl font-semibold">{titulo}</h2>
       <p className="text-sm text-gray-400">{descripcion}</p>
       <div className="text-sm text-gray-400">
         <p>Inicio: <span className="text-gray-200">{formatDate(fecha_inicio)}</span></p>
@@ -74,6 +75,12 @@ const CardSorteo = (props) => {
           className="flex-1 bg-neutral-700 hover:bg-neutral-600 transition-colors duration-300 text-white py-2 rounded-md flex items-center justify-center"
         >
           <FaRegTrashAlt />
+        </button>
+        <button
+          onClick={() => navigate(`/participantes-sorteo/${id}`)}
+          className="flex-1 bg-blue-500 hover:bg-blue-400 transition-colors duration-300 text-white py-2 rounded-md flex items-center justify-center"
+        >
+          <FaUsers />
         </button>
         <button
           onClick={handleSeleccionarGanador}
