6ffdaabf5c0df85a5d50b15c9f619652b2f37e41
alvaroofernaandez <alvaroofernaandez@gmail.com>
2025-05-03 16:20:46 +0200

    feat: mejorar diseño y funcionalidad de la tarjeta de sorteos; actualizar estilos y lógica de botones

--- dashboard/src/components/Sorteos/RaffleCard.jsx
+++ dashboard/src/components/Sorteos/RaffleCard.jsx
@@ -1,8 +1,7 @@
 import { useState } from "react";
-import { FaRegTrashAlt } from "react-icons/fa";
+import { FaRegTrashAlt, FaGift } from "react-icons/fa";
 import { AiFillEdit } from "react-icons/ai";
 import { GiPodiumWinner } from "react-icons/gi";
-import { FaGift } from "react-icons/fa6";
 import Modal from "../General/Modal";
 import { z } from "zod";
 import { format } from "date-fns";
@@ -37,147 +36,58 @@ const CardSorteo = (props) => {
 
   const { eliminarSorteo, asignarPremio } = useSorteos();
   const [showModal, setShowModal] = useState(false);
-  const [hoveredButton, setHoveredButton] = useState("");
 
   const formatDate = (fecha) =>
-    format(new Date(fecha), "dd 'de' MMMM 'de' yyyy", { locale: es });
+    format(new Date(fecha), "dd/MM/yyyy", { locale: es });
 
   const handleEliminarSorteo = async () => {
     const success = await eliminarSorteo(id);
-    if (success) {
-      setTimeout(() => {
-        window.location.reload();
-      }, 200);
-    }
+    if (success) window.location.reload();
   };
 
   const handleSeleccionarGanador = async () => {
     const success = await asignarPremio(id, "ganador");
-    if (success) {
-      setTimeout(() => {
-        window.location.reload();
-      }, 1000);
-    }
+    if (success) window.location.reload();
   };
 
   return (
-    <div className="dark:bg-neutral-900 bg-neutral-500 rounded-2xl shadow-lg p-6 hover:shadow-xl transition-shadow flex flex-col justify-between h-full">
-      <div>
-        <h2 className="text-4xl font-bold text-white text-center mb-2">{titulo}</h2>
-        <hr className="dark:border-neutral-700 mb-6" />
-        <p className="text-gray-300 mb-4">Descripción: {descripcion}</p>
-        <hr className="dark:border-neutral-700 mb-6" />
-
-        <div className="grid gap-3 justify-between text-sm text-gray-400 mb-4">
-          <span>
-            🕒 Fecha de inicio:{" "}
-            <strong className="text-gray-200">{formatDate(fecha_inicio)}</strong>
-          </span>
-          <span>
-            🕛 Fecha de finalización:{" "}
-            <strong className="text-gray-200">{formatDate(fecha_fin)}</strong>
-          </span>
-        </div>
-
-        <p className="text-gray-200 font-medium mb-4">
-          🔷 Estado del sorteo:{" "}
-          <span className="text-green-400 font-semibold">{estado}</span>
-        </p>
-
-        <p className="text-gray-200 font-medium mb-4">
-          🎉 Ganador:{" "}
-          <span className="text-green-400 font-semibold">
-            {ganador || "Pendiente"}
-          </span>
-        </p>
-
-        <div className="mb-4">
-          <h3 className="text-md font-semibold text-white mb-1">🎁 Premios:</h3>
-          <ul className="list-disc list-inside text-gray-300">
-            {premios.length > 0 ? (
-              premios.map((premio, index) => <li key={index}>{premio}</li>)
-            ) : (
-              <li>No hay premios disponibles</li>
-            )}
-          </ul>
-        </div>
-
-        <div className="flex justify-between items-center">
-          <h3 className="text-md font-semibold text-white mb-1">
-            👥 Nº de participantes: {participantes.length}
-          </h3>
-          <a
-            href={`/participantes-sorteo/${id}`}
-            className="bg-neutral-600 dark:bg-neutral-700 text-white dark:hover:bg-neutral-600 p-1 pl-3 pr-3 rounded-lg hover:bg-neutral-700 transition-all duration-300"
-          >
-            Ver detalles
-          </a>
-        </div>
+    <div className="bg-gradient-to-br from-neutral-600 to-neutral-500 dark:from-neutral-900 dark:to-neutral-800 hover:scale-105 shadow-lg transition-transform duration-300 text-white rounded-lg p-6 flex flex-col gap-4">
+      <h2 className="text-xl font-semibold">{titulo}</h2>
+      <p className="text-sm text-gray-400">{descripcion}</p>
+      <div className="text-sm text-gray-400">
+        <p>Inicio: <span className="text-gray-200">{formatDate(fecha_inicio)}</span></p>
+        <p>Fin: <span className="text-gray-200">{formatDate(fecha_fin)}</span></p>
+        <p>Estado: <span className="text-green-400 font-extrabold">{estado}</span></p>
+        <p>Ganador: <span className="text-green-400 font-extrabold">{ganador || "Pendiente"}</span></p>
+        <p>Premios: <span className="text-gray-200">{premios.length || "Ninguno"}</span></p>
+        <p>Participantes: <span className="text-gray-200">{participantes.length}</span></p>
       </div>
-
-      <div className="flex gap-4 mt-10 relative">
-        <div className="relative flex flex-col items-center w-full">
-          <button
-            onMouseEnter={() => setHoveredButton("Editar sorteo")}
-            onMouseLeave={() => setHoveredButton("")}
-            onClick={() => navigate(`/actualizar-sorteo/${id}`)}
-            className="bg-neutral-400 hover:scale-105 transition-all duration-500 w-full text-white size-10 items-center justify-items-center rounded-lg"
-          >
-            <AiFillEdit className="size-6" />
-          </button>
-          {hoveredButton === "Editar sorteo" && (
-            <div className="absolute bottom-12 bg-neutral-800 text-white text-xs py-1 px-2 rounded-md">
-              {hoveredButton}
-            </div>
-          )}
-        </div>
-        <div className="relative flex flex-col items-center w-full">
-          <button
-            onMouseEnter={() => setHoveredButton("Eliminar sorteo")}
-            onMouseLeave={() => setHoveredButton("")}
-            className="bg-neutral-800 hover:scale-105 transition-all duration-500 w-full text-white size-10 items-center justify-items-center rounded-lg"
-            onClick={() => setShowModal(true)}
-          >
-            <FaRegTrashAlt className="size-6" />
-          </button>
-          {hoveredButton === "Eliminar sorteo" && (
-            <div className="absolute bottom-12 bg-neutral-800 text-white text-xs py-1 px-2 rounded-md">
-              {hoveredButton}
-            </div>
-          )}
-        </div>
-        <div className="relative flex flex-col items-center w-full">
-          <button
-            onMouseEnter={() => setHoveredButton("Finalizar sorteo")}
-            onMouseLeave={() => setHoveredButton("")}
-            className="bg-green-500 hover:scale-105 transition-all duration-500 w-full text-white size-10 items-center justify-items-center rounded-lg"
-            onClick={handleSeleccionarGanador}
-          >
-            <GiPodiumWinner className="size-6" />
-          </button>
-          {hoveredButton === "Finalizar sorteo" && (
-            <div className="absolute bottom-12 bg-neutral-800 text-white text-xs py-1 px-2 rounded-md">
-              {hoveredButton}
-            </div>
-          )}
-        </div>
-        <div className="relative flex flex-col items-center w-full">
-          <button
-            onMouseEnter={() => setHoveredButton("Asig. premios")}
-            onMouseLeave={() => setHoveredButton("")}
-            className="bg-yellow-500 hover:scale-105 transition-all duration-500 w-full text-white size-10 items-center justify-items-center rounded-lg"
-            onClick={() => navigate(`/asignar-premio/${id}`)}
-          >
-            <FaGift className="size-6" />
-          </button>
-          {hoveredButton === "Asig. premios" && (
-            <div className="absolute bottom-12 bg-neutral-800 text-white text-xs py-1 px-2 rounded-md">
-              {hoveredButton}
-            </div>
-          )}
-        </div>
+      <div className="flex gap-2 mt-4">
+        <button
+          onClick={() => navigate(`/actualizar-sorteo/${id}`)}
+          className="flex-1 bg-neutral-500 hover:bg-neutral-400 transition-colors duration-300 text-white py-2 rounded-md flex items-center justify-center"
+        >
+          <AiFillEdit />
+        </button>
+        <button
+          onClick={() => setShowModal(true)}
+          className="flex-1 bg-neutral-700 hover:bg-neutral-600 transition-colors duration-300 text-white py-2 rounded-md flex items-center justify-center"
+        >
+          <FaRegTrashAlt />
+        </button>
+        <button
+          onClick={handleSeleccionarGanador}
+          className="flex-1 bg-green-600 hover:bg-green-500 transition-colors duration-300 text-white py-2 rounded-md flex items-center justify-center"
+        >
+          <GiPodiumWinner />
+        </button>
+        <button
+          onClick={() => navigate(`/asignar-premio/${id}`)}
+          className="flex-1 bg-yellow-500 hover:bg-yellow-400 transition-colors duration-300 text-white py-2 rounded-md flex items-center justify-center"
+        >
+          <FaGift />
+        </button>
       </div>
-
       {showModal && (
         <Modal
           setShowModal={setShowModal}
