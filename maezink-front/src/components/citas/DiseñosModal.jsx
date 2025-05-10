2b285f4baa0702e0007e7e2e4d9731797028dcbf
alvaroofernaandez <alvaroofernaandez@gmail.com>
2025-05-10 20:37:53 +0200

    fix: eliminar código redundante en el componente DiseñosModal

--- maezink-front/src/components/citas/DiseñosModal.jsx
+++ maezink-front/src/components/citas/DiseñosModal.jsx
@@ -15,7 +15,7 @@ const DiseñosModal = ({ isOpen, onClose }) => {
             className="fixed inset-0  bg-black/70 flex items-center justify-center z-50"
             onClick={handleBackdropClick}
         >
-            <div className="relative max-w-3xl w-full h-[75%] mx-4 bg-gradient-to-tl border border-neutral-800 from-neutral-800 via-neutral-900 to-neutral-950 p-6 rounded-lg">
+            <div className="relative max-w-3xl w-full h-[78%] mx-4 bg-gradient-to-tl border border-neutral-800 from-neutral-800 via-neutral-900 to-neutral-950 p-6 rounded-lg">
                 <button
                     className="absolute top-2 right-4 text-white text-3xl font-bold hover:text-red-400 transition"
                     onClick={onClose}
@@ -26,7 +26,7 @@ const DiseñosModal = ({ isOpen, onClose }) => {
                     <h1 className='text-2xl' >Selecciona un diseño</h1>
                     <hr className='border border-neutral-500'/>
 
-                    <div className='flex columns-3 gap-4 items-center justify-center h-[33rem] md:h-[45rem] lg:h-[35rem] flex-wrap mt-10 overflow-y-scroll'>
+                    <div className='flex columns-3 gap-4 items-center justify-center h-[35rem] md:h-[45rem] lg:h-[35rem] flex-wrap mt-10 overflow-y-scroll'>
                         <CardDiseñoModal />
                         <CardDiseñoModal />
                         <CardDiseñoModal />
